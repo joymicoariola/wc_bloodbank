b9035ebce129b40105e9fc5ec37fe3c8b500d890
JoyMico Ariola <arioljoy@flip1.engr.oregonstate.edu>
2021-05-20 19:10:10 -0700

    5/20/21 Step 5

--- app.py
+++ app.py
@@ -40,29 +40,70 @@ def add_new_donor():
     result = execute_query(db_connection, query, data)
     return render_template("donors.j2")
 
-@app.route('/Phlebotomists')
+@app.route('/phlebotomists')
 def phlebotomists():
+    return render_template("phlebotomists.j2")
+
+@app.route('/add_new_phlebotomist', methods=['POST', 'GET'])
+def add_new_phlebotomist():
+    db_connection = connect_to_database()
+    fname = request.form['firstName']
+    lname = request.form['lastName']
+    licenseExp = request.form['licenseExpiration']
+    phone = request.form['phone']
+    bankID = request.form['bankID']
+    query = "INSERT INTO Phlebotomist (firstName, lastName, licenseExpiration, phone, bankID) VALUES (%s, %s, %s, %s, %s)"
+    data = (fname, lname, licenseExp, phone, bankID)
+    result = execute_query(db_connection, query, data)
     return render_template("phlebotomists.j2")
 
 @app.route('/schedules')
 def schedules():
     return render_template("schedules.j2")
 
+@app.route('/add_new_schedule', methods=['POST', 'GET'])
+def add_new_schedule():
+    db_connection = connect_to_database()
+    date = request.form['scheduleDate']
+    startTime = request.form['startTime']
+    endTime = request.form['endTime']
+    query = "INSERT INTO Schedules (scheduleDate, startTime, endTime) VALUES (%s, %s, %s)"
+    data = (date, startTime, endTime)
+    result = execute_query(db_connection, query, data)
+    return render_template("schedules.j2")
+
 @app.route('/banks')
 def banks():
     return render_template("banks.j2")
 
-@app.route('/donors_schedules')
-def donors_schedules():
-    return render_template("donors_schedules.j2")
+@app.route('/banks', methods=['POST', 'GET'])
+def add_new_banks():
+    db_connection = connect_to_database()
+    bankName = request.form['bankName']
+    address = request.form['bankAddress']
+    city = request.form['city']
+    state = request.form['state']
+    zip = request.form['zip']
+    phone = request.form['phone']
+    query = "INSERT INTO Banks (bankName, bankAddress, city, state, zip, phone) VALUES (%s, %s, %s, %s, %s, %s)"
+    data = (bankName, address, city, state, zip, phone)
+    result = execute_query(db_connection, query, data)
+    return render_template("banks.j2")
 
-@app.route('/phlebotomists_schedules')
-def phlebotomists_schedules():
-    return render_template("phlebotomists_schedules.j2")
+@app.route('/donations')
+def donations():
+    return render_template("donations.j2")
 
-@app.route('/phlebotomists_schedules')
-def donors_schedules():
-    return render_template("phlebotomists_schedules.j2", people=people_from_app_py)
+@app.route('/add_new_donations', methods=['POST', 'GET'])
+def add_new_donations():
+    db_connection = connect_to_database()
+    scheduleID = request.form['scheduleID']
+    phlebotomistID = request.form['phlebotomistID']
+    donorID = request.form['donorID']
+    query = "INSERT INTO Donations (scheduleID, phlebotomistID, donorID) VALUES (%s, %s, %s)"
+    data = (scheduleID, phlebotomistID, donorID)
+    result = execute_query(db_connection, query, data)
+    return render_template("donations.j2")
 
 # Listener
 
